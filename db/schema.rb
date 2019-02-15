46ca75c2ad4c785c715e54af0e0ab48df95467ef
Guillaume Ecollan <guillaume@ecollan.fr>
2019-02-15 11:14:26 +0100

    Add photo to Cocktails

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_02_14_104806)
+ActiveRecord::Schema.define(version: 2019_02_15_101117) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 2019_02_14_104806)
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "photo"
   end
 
   create_table "doses", force: :cascade do |t|
